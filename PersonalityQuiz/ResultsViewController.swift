6841f31ad95b66c472acbc37bb3aa0cdebebdef6
Vlad Pavlenko <pavlenko0902@gmail.com>
2019-07-19 17:37:16 +0300

    init commit

---
+++ PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Владислав Павленко on 7/3/19.
+//  Copyright © 2019 Владислав Павленко. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    
+    var responses: [Answer]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.hidesBackButton = true
+        calculatePersonalityResult()
+    }
+    
+    func calculatePersonalityResult() {
+        var frequencyOfAnswers: [AnimalType: Int] = [:]
+        let responseTypes = responses.map { $0.type }
+        
+        for response in responseTypes {
+            frequencyOfAnswers[response] = (frequencyOfAnswers[response] ?? 0) + 1
+        }
+        
+        let frequentAnswersSorted = frequencyOfAnswers.sorted(by: { (pair1, pair2) -> Bool in
+            return pair1.value > pair2.value
+        })
+        
+        let mostCommonAnswer = frequentAnswersSorted.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+    }
+
+}
